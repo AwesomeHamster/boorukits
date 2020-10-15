db05305eb267cfff8cdded10576c71c5ee4e3e79
Maiko Tan <maiko.tan.coding@gmail.com>
2020-10-15 14:23:07 +0800

    fix: import error

--- src/boorukits/__init__.py
+++ src/boorukits/__init__.py
@@ -1,6 +1,7 @@
 from .booru import Booru, BooruImage
 from .danbooru import Danbooru, DanbooruImage
-from .gelbooru import (Gelbooru, GelbooruImage, Safebooru, SafebooruImage)
+from .gelbooru import Gelbooru, GelbooruImage
+from .safebooru import Safebooru, SafebooruImage
 from .errors import BooruError, InvalidResponseError
 
 __all__ = [
