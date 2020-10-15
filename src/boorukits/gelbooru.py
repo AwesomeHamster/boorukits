50ca8dbb4e0181d188f2919a172208ee0f3c4dd9
Maiko Tan <maiko.tan.coding@gmail.com>
2020-10-15 10:06:20 +0800

    refactor: move safebooru to new file

--- src/boorukits/gelbooru.py
+++ src/boorukits/gelbooru.py
@@ -4,8 +4,6 @@ from .booru import Booru, BooruImage
 
 GELBOORU_API_URL = "https://gelbooru.com/"
 
-SAFEBOORU_API_URL = "https://safebooru.org/"
-
 
 class GelbooruImage(BooruImage):
 
@@ -92,86 +90,3 @@ class Gelbooru(Booru):
             "user_id": self._user,
             "api_key": self._token,
         })
-
-
-class SafebooruImage(GelbooruImage):
-
-    @property
-    def file_url(self):
-        return SAFEBOORU_API_URL + \
-            self._data_dict.get("directory", "") + \
-            "/" + \
-            self._data_dict.get("image", "")
-
-
-class Safebooru(Gelbooru):
-    """Wrap API of Safebooru (https://safebooru.org/)
-
-    Safebooru is the same as gelbooru,
-    so we just inherit it.
-    """
-
-    def __init__(
-        self,
-        user: str = None,
-        token: str = None,
-        root_url: str = SAFEBOORU_API_URL,
-        proxy: Optional[str] = None,
-        loop=None,
-    ) -> None:
-        super().__init__(user=user,
-            token=token,
-            root_url=root_url,
-            proxy=proxy,
-            loop=loop)
-
-    async def get_post(self, id: str = "") -> Union[SafebooruImage, None]:
-        params = {
-            "page": "dapi",
-            "s": "post",
-            "q": "index",
-            "json": 1,
-            "id": id,
-        }
-
-        params = self._add_api_key(params)
-
-        code, response = await self._get(self._root_url + "/index.php",
-            params=params)
-
-        # gelbooru would return a list even specify an id.
-        res_image = response[0]
-        return SafebooruImage(str(res_image.get("id", "-1")), res_image)
-
-    async def get_posts(
-        self,
-        tags: str = "",
-        page: int = None,
-        limit: int = None,
-        **kwargs,
-    ) -> Union[List[SafebooruImage], None]:
-        params = {
-            "page": "dapi",
-            "s": "post",
-            "q": "index",
-            "tags": tags,
-            "json": 1,
-        }
-
-        params = self._add_api_key(params)
-
-        params = self._fill_dict(params, {
-            "pid": page,
-            "limit": limit,
-        })
-
-        code, response = await self._get(self._root_url + "/index.php",
-            params=params,
-            **kwargs)
-
-        res_list = list()
-        for i in response:
-            # some post may lacks "id" property,
-            # default to "-1".
-            res_list.append(SafebooruImage(str(i.get("id", "-1")), i))
-        return res_list
